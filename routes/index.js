f3ab43a42d5783f32d2156d6d227754115ecab1a
Lucas <lucasgabrielhonorio@gmail.com>
2018-02-13 21:15:54 -0200

    add: routes to csvController

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var csvController = require('../controllers/csvController'); 
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+
+    cb(null, file.fieldname + '.csv')
+  }
+})
+
+var upload = multer({ storage: storage })
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSV TO JSON' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
 });
+
+
+router.post('/csv-to-json', upload.single('csv-file'), csvController.csvToJson);
+
 
 module.exports = router;
